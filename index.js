691056aa4d4236246ebbc2ac0a1a901294f22b15
besartshyti <besartshyti@gmail.com>
2015-06-28 17:48:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var window       = require("./lib/window.js");
+var pathToRegexp = require('path-to-regexp');
+var queryString  = require('query-string');
+
+module.exports = router;
+
+function router (state) {
+
+	// browsers only
+	if (!window) return;
+
+	var loc = window.location;
+	var win = window;
+
+	function hash () {
+		if (loc.hash[0] === "/") {
+			return loc.hash;
+		} else {
+			return ('/' + loc.hash);
+		}
+	}
+
+	function getQueryString (url) {
+		return url.split("?")[1] || "";
+	}
+
+	function paramsParse (path) {
+
+		var keys  = [];
+		var re    = pathToRegexp(path, keys);
+		var match = re.exec(hash());
+
+		if (!match) {
+			return undefined;
+		} else if (match[1]) {
+			return match[1].split("?")[0];
+		} else {
+			return match[0];
+		}
+	}
+
+	window.addEventListener("hashchange", function () {
+
+		state.route.set(hash());
+	});
+
+	function route (path, handler) {
+
+		var o         = {};
+		o.params      = paramsParse(path);
+		o.parsedQuery = queryString.parse(getQueryString(hash()));
+
+		if(o.params) {
+			return handler(state, o);
+		}
+	}
+	
+	return route;
+}
---
+++ test/index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var window = require("../lib/window.js");
+var observ = require("observ");
+
+var test = require("tape");
+
+var router = require("../");
+
+test("Router should be a function", function (t) {
+
+	t.equals(typeof router, "function",  "is a function");
+	t.end();
+});
+
+test("Router globals", function t (assert) {
+
+	var state = {
+		route: observ("")
+	};
+	
+	var route = router(state);
+
+	route("/", home);
+
+
+	function home (stateInjected) {
+		assert.pass("function called");
+		assert.end();
+	}
+});
+
+test("Router globals", function t (assert) {
+
+	window.location.hash = "/user";
+
+	var state = {
+		route: observ("")
+	};
+	
+	var route = router(state);
+
+	route("/",     home);
+	route("/user", user);
+
+	function home (stateInjected) {
+		assert.fail("should not have been called");
+		assert.end();
+	}
+
+	function user (stateInjected) {
+		assert.pass("function called");
+		assert.end();
+	}
+});
+
+test("handler function gets params and query string", function t (assert) {
+
+	window.location.hash = "/user/22?name=bes";
+
+	var state = {
+		route: observ("")
+	};
+	
+	var route = router(state);
+
+	route("/",         home);
+	route("/user/:id", user);
+
+	function home (stateInjected) {
+		assert.fail("should not have been called");
+		assert.end();
+	}
+
+	function user (stateInjected, params) {
+		assert.pass("function called");
+		assert.equals(params.params, "22", "get user id");
+		assert.equals(params.parsedQuery.name, "bes", "get query string");
+		assert.end();
+	}
+});
